ccf9e1ddbfd7fff66b6dabf08cb6b8bb3a82a883
Adi-Vaitis <adriangabriel344@gmail.com>
2023-09-15 15:47:20 +0200

    added measurements for the next 24 hours

--- src/utils/date.utils.js
+++ src/utils/date.utils.js
@@ -3,3 +3,15 @@ export function isSameDay(date1, date2) {
     const convertedDate2 = new Date(date2);
     return convertedDate1.toDateString() === convertedDate2.toDateString();
 }
+
+export function addHoursToDate(date, hours){
+    const dateObj = new Date(date);
+    return new Date(dateObj.setHours(dateObj.getHours() + hours));
+}
+
+export function isDateBetween(checkDate, dateFrom, dateTo){
+    checkDate = new Date(checkDate);
+    const dummy =  checkDate.valueOf() >= dateFrom.valueOf() && checkDate.valueOf() <= dateTo.valueOf();
+    console.log(dummy);
+    return dummy;
+}
